6366f232f33fc5c5226cf3a8b23d22ddd633704a
Joe Ray Gregory <j.gregory@bichinger.de>
2016-12-23 08:44:04 +0100

    Adding current article as disabled to dropdown

--- TL_ROOT/system/modules/quickArticleSwitch/dca/tl_content.php
+++ TL_ROOT/system/modules/quickArticleSwitch/dca/tl_content.php
@@ -1,5 +1,5 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_content']['list']['global_operations']['quickArticleSwitch'] = array(
-  'button_callback' => array('May17\quickArticleSwitch\Classes\Backend\ArticleSwitch', 'addSwitch')
+    'button_callback' => array('May17\quickArticleSwitch\Classes\Backend\ArticleSwitch', 'addSwitch')
 );
